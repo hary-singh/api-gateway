949b3413233cbede4f176edd1f3cb53515cca41e
Harry Dhillon <39445797+hary-singh@users.noreply.github.com>
2024-07-27 18:15:09 -0600

    Created User model, controller, service and repo. This relates to #2 (#12)

---
+++ src/main/java/com/gradgate/api_gateway/user/UserController.java
@@ -0,0 +1,19 @@
+package com.gradgate.api_gateway.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(User user) {
+        return userService.registerUser(user);
+    }
+}
---
+++ src/main/java/com/gradgate/api_gateway/user/UserService.java
@@ -0,0 +1,17 @@
+package com.gradgate.api_gateway.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+}
